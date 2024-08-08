d9ca7d2df67536ad003dc27c99f5185fe83fef3f
Еркин Ж. Ибрагимов <ibragimov.y@ad.loc>
2024-08-08 12:58:43 +0500

    fix test

--- src/app/article/article.service.spec.ts
+++ src/app/article/article.service.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { ArticleService } from './article.service';
 import { Article } from './entity/article.entity';
 import { ArticleDto } from './dto/article.dto';
 import { IAuth } from 'src/common/auth.decorator';
+import { AllArticleQuery } from './dto/all-article.dto';
+import { HttpException } from '@nestjs/common';
 
 describe('ArticleService', () => {
   let service: ArticleService;
@@ -102,173 +104,182 @@ describe('ArticleService', () => {
     });
   });
 
-  // describe('getAll', () => {
-  //   it('should return articles based on the query', async () => {
-  //     const auth: IAuth = {
-  //       id: 1,
-  //       login: '',
-  //     };
-  //     const query: AllArticleQuery = {
-  //       author: 'John Doe',
-  //       startDate: '2023-01-01',
-  //       endDate: '2023-12-31',
-  //       page: 1,
-  //       take: 10,
-  //     };
+  describe('getAll', () => {
+    it('should return articles based on the query', async () => {
+      const auth: IAuth = {
+        id: 1,
+        login: 'test',
+      };
+      const query: AllArticleQuery = {
+        author: 'Erka',
+        startDate: new Date('2024-06-08T02:11:55.777Z'),
+        endDate: new Date('2024-10-08T02:11:55.777Z'),
+        page: 1,
+        take: 10,
+      };
 
-  //     jest.spyOn(articleRepository, 'find').mockResolvedValue([
-  //       {
-  //         id: 1,
-  //         name: 'Test Article 1',
-  //         author: 'John Doe',
-  //         description: 'This is a test article.',
-  //         auth: {
-  //           id: auth.id,
-  //           created_at: undefined,
-  //           updated_at: undefined,
-  //           login: '',
-  //           password: '',
-  //           articles: [],
-  //         },
-  //         created_at: new Date('2023-05-01'),
-  //       },
-  //       {
-  //         id: 2,
-  //         name: 'Test Article 2',
-  //         author: 'John Doe',
-  //         description: 'This is another test article.',
-  //         auth: {
-  //           id: auth.id,
-  //           created_at: undefined,
-  //           updated_at: undefined,
-  //           login: '',
-  //           password: '',
-  //           articles: [],
-  //         },
-  //         created_at: new Date('2023-06-15'),
-  //       },
-  //     ]);
+      jest.spyOn(articleRepository, 'find').mockResolvedValue([
+        {
+          id: 1,
+          name: 'Test Article 1',
+          author: 'John Doe',
+          description: 'This is a test article.',
+          auth: {
+            id: auth.id,
+            created_at: undefined,
+            updated_at: undefined,
+            login: '',
+            password: '',
+            articles: [],
+          },
+          created_at: new Date('2024-06-10T02:11:55.777Z'),
+          updated_at: new Date('2024-06-10T02:11:55.777Z'),
+        },
+        {
+          id: 2,
+          name: 'Test Article 2',
+          author: 'John Doe',
+          description: 'This is another test article.',
+          auth: {
+            id: auth.id,
+            created_at: undefined,
+            updated_at: undefined,
+            login: '',
+            password: '',
+            articles: [],
+          },
+          created_at: new Date('2024-06-10T02:11:55.777Z'),
+          updated_at: new Date('2024-06-10T02:11:55.777Z'),
+        },
+      ]);
 
-  //     const result = await service.getAll(auth, query);
+      const result = await service.getAll(auth, query);
 
-  //     expect(articleRepository.find).toHaveBeenCalledWith({
-  //       where: {
-  //         author: query.author,
-  //         auth: { id: auth.id },
-  //         created_at: Between(new Date('2023-01-01'), new Date('2023-12-31')),
-  //       },
-  //       skip: (query.page - 1) * query.take,
-  //       take: query.take,
-  //     });
-  //     expect(result).toHaveLength(2);
-  //   });
-  // });
+      expect(articleRepository.find).toHaveBeenCalledWith({
+        where: {
+          author: query.author,
+          auth: { id: auth.id },
+          created_at: Between(
+            new Date('2024-06-08T02:11:55.777Z'),
+            new Date('2024-10-08T02:11:55.777Z'),
+          ),
+        },
+        skip: (query.page - 1) * query.take,
+        take: query.take,
+      });
+      expect(result).toHaveLength(2);
+    });
+  });
 
-  // describe('delete', () => {
-  //   it('should delete an article', async () => {
-  //     const auth: IAuth = {
-  //       id: 1,
-  //       login: '',
-  //     };
-  //     const articleId = 1;
+  describe('delete', () => {
+    it('should delete an article', async () => {
+      const auth: IAuth = {
+        id: 1,
+        login: 'test',
+      };
+      const articleId = 1;
 
-  //     jest.spyOn(articleRepository, 'findOneBy').mockResolvedValue({
-  //       id: articleId,
-  //       name: 'Test Article',
-  //       author: 'John Doe',
-  //       description: 'This is a test article.',
-  //       auth: {
-  //         id: auth.id,
-  //         created_at: undefined,
-  //         updated_at: undefined,
-  //         login: '',
-  //         password: '',
-  //         articles: [],
-  //       },
-  //     });
-  //     jest.spyOn(articleRepository, 'remove').mockResolvedValue(undefined);
+      jest.spyOn(articleRepository, 'findOneBy').mockResolvedValue({
+        id: articleId,
+        name: 'Test Article',
+        author: 'Erka',
+        description: 'This is a test article.',
+        created_at: undefined,
+        updated_at: undefined,
+        auth: {
+          id: auth.id,
+          created_at: undefined,
+          updated_at: undefined,
+          login: '',
+          password: '',
+          articles: [],
+        },
+      });
+      jest.spyOn(articleRepository, 'remove').mockResolvedValue(undefined);
 
-  //     const result = await service.delete(auth, articleId);
+      const result = await service.delete(auth, articleId);
 
-  //     expect(articleRepository.findOneBy).toHaveBeenCalledWith({
-  //       id: articleId,
-  //       auth: { id: auth.id },
-  //     });
-  //     expect(articleRepository.remove).toHaveBeenCalled();
-  //     expect(result).toEqual({ message: true });
-  //   });
+      expect(articleRepository.findOneBy).toHaveBeenCalledWith({
+        id: articleId,
+        auth: { id: auth.id },
+      });
+      expect(articleRepository.remove).toHaveBeenCalled();
+      expect(result).toEqual({ message: true });
+    });
 
-  //   it('should throw an exception if the article is not found', async () => {
-  //     const auth: IAuth = {
-  //       id: 1,
-  //       login: '',
-  //     };
-  //     const articleId = 1;
+    it('should throw an exception if the article is not found', async () => {
+      const auth: IAuth = {
+        id: 1,
+        login: '',
+      };
+      const articleId = 1;
 
-  //     jest.spyOn(articleRepository, 'findOneBy').mockResolvedValue(null);
+      jest.spyOn(articleRepository, 'findOneBy').mockResolvedValue(null);
 
-  //     await expect(service.delete(auth, articleId)).rejects.toThrow(
-  //       HttpException,
-  //     );
-  //   });
-  // });
+      await expect(service.delete(auth, articleId)).rejects.toThrow(
+        HttpException,
+      );
+    });
+  });
 
-  // describe('update', () => {
-  //   it('should update an article', async () => {
-  //     const auth: IAuth = {
-  //       id: 1,
-  //       login: '',
-  //     };
-  //     const articleId = 1;
-  //     const data: ArticleDto = {
-  //       name: 'Updated Article',
-  //       author: 'Jane Doe',
-  //       description: 'This is an updated article.',
-  //     };
+  describe('update', () => {
+    it('should update an article', async () => {
+      const auth: IAuth = {
+        id: 1,
+        login: '',
+      };
+      const articleId = 1;
+      const data: ArticleDto = {
+        name: 'Updated Article',
+        author: 'Jane Doe',
+        description: 'This is an updated article.',
+      };
 
-  //     jest.spyOn(articleRepository, 'findOneBy').mockResolvedValue({
-  //       id: articleId,
-  //       name: 'Test Article',
-  //       author: 'John Doe',
-  //       description: 'This is a test article.',
-  //       auth: {
-  //         id: auth.id,
-  //         created_at: undefined,
-  //         updated_at: undefined,
-  //         login: '',
-  //         password: '',
-  //         articles: [],
-  //       },
-  //     });
-  //     jest.spyOn(articleRepository, 'save').mockResolvedValue(undefined);
+      jest.spyOn(articleRepository, 'findOneBy').mockResolvedValue({
+        id: articleId,
+        name: 'Test Article',
+        author: 'Erka',
+        description: 'This is a test article.',
+        created_at: undefined,
+        updated_at: undefined,
+        auth: {
+          id: auth.id,
+          created_at: undefined,
+          updated_at: undefined,
+          login: '',
+          password: '',
+          articles: [],
+        },
+      });
+      jest.spyOn(articleRepository, 'save').mockResolvedValue(undefined);
 
-  //     const result = await service.update(auth, articleId, data);
+      const result = await service.update(auth, articleId, data);
 
-  //     expect(articleRepository.findOneBy).toHaveBeenCalledWith({
-  //       id: articleId,
-  //       auth: { id: auth.id },
-  //     });
-  //     expect(articleRepository.save).toHaveBeenCalled();
-  //     expect(result).toEqual({ message: true });
-  //   });
+      expect(articleRepository.findOneBy).toHaveBeenCalledWith({
+        id: articleId,
+        auth: { id: auth.id },
+      });
+      expect(articleRepository.save).toHaveBeenCalled();
+      expect(result).toEqual({ message: true });
+    });
 
-  //   it('should throw an exception if the article is not found', async () => {
-  //     const auth: IAuth = {
-  //       id: 1,
-  //       login: '',
-  //     };
-  //     const articleId = 1;
-  //     const data: ArticleDto = {
-  //       name: 'Updated Article',
-  //       author: 'Jane Doe',
-  //       description: 'This is an updated article.',
-  //     };
+    it('should throw an exception if the article is not found', async () => {
+      const auth: IAuth = {
+        id: 1,
+        login: '',
+      };
+      const articleId = 1;
+      const data: ArticleDto = {
+        name: 'Updated Article',
+        author: 'Jane Doe',
+        description: 'This is an updated article.',
+      };
 
-  //     jest.spyOn(articleRepository, 'findOneBy').mockResolvedValue(null);
+      jest.spyOn(articleRepository, 'findOneBy').mockResolvedValue(null);
 
-  //     await expect(service.update(auth, articleId, data)).rejects.toThrow(
-  //       HttpException,
-  //     );
-  //   });
-  // });
+      await expect(service.update(auth, articleId, data)).rejects.toThrow(
+        HttpException,
+      );
+    });
+  });
 });
